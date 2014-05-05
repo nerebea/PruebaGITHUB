01b1a3ee7e03d4046a2f4389fefe2ac06f48f253
Nocturno <Nocturno@14-C7>
2014-05-05 16:58:46 +0200

    Clase Nueva

---
+++ PruebaGITHUB/src/NereBea/ModificadaPorAmbos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NereBea;
+
+/**
+ *
+ * @author Nocturno
+ */
+public class ModificadaPorAmbos {
+    public void metodo(){
+        System.out.println("Creamos un metodo");
+    }
+    
+}
